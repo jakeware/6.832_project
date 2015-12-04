18dd43fc0ec3002b5a9c4268caf75df1a88bfac8
jakeware <jakeware@mit.edu>
2015-12-03 23:16:43 -0500

    Initial commit.

---
+++ Quadrotor.m
@@ -0,0 +1,58 @@
+classdef Quadrotor < RigidBodyManipulator
+  
+  methods
+    function obj = Quadrotor()
+      options.floating = true; 
+      options.terrain = RigidBodyFlatTerrain();
+      w = warning('off','Drake:RigidBodyManipulator:ReplacedCylinder');
+      warning('off','Drake:RigidBodyManipulator:UnsupportedContactPoints');
+      obj = obj@RigidBodyManipulator(getFullPathFromRelativePath('quadrotor.urdf'),options);
+      warning(w);
+      
+      obj = compile(obj);
+    end
+   
+    function I = getInertia(obj)
+      I = obj.body(2).inertia;
+    end
+    
+    function u0 = nominalThrust(obj)
+      % each propellor commands -mg/4
+      u0 = Point(getInputFrame(obj),getMass(obj)*norm(getGravity(obj))*ones(4,1)/4);
+    end
+    
+    function obj = addObstacle(obj,size,xyz,rpy)
+
+      geometry = RigidBodyBox(size,xyz,rpy);
+      geometry.c = 0.5*ones(3,1);
+      obj = addGeometryToBody(obj,'world',geometry);
+      
+      obj = compile(obj);
+    end
+    
+    function traj_opt = addPlanVisualizer(obj,traj_opt)
+      % spew out an lcmgl visualization of the trajectory.  intended to be
+      % used as a callback (fake objective) in the direct trajectory
+      % optimization classes
+
+      if ~checkDependency('lcmgl')
+        warning('lcmgl dependency is missing.  skipping visualization'); 
+        return;
+      end
+      lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'QuadrotorPlan');
+      
+      typecheck(traj_opt,'DirectTrajectoryOptimization');
+
+      traj_opt = traj_opt.addDisplayFunction(@(x)visualizePlan(x,lcmgl),traj_opt.x_inds(1:3,:));
+      
+      function visualizePlan(x,lcmgl)
+        lcmgl.glColor3f(1, 0, 0);
+        lcmgl.glPointSize(3);
+        lcmgl.points(x(1,:),x(2,:),x(3,:));
+        lcmgl.glColor3f(.5, .5, 1);
+        lcmgl.plot3(x(1,:),x(2,:),x(3,:));
+        lcmgl.switchBuffers;
+      end
+    end
+  end
+end
